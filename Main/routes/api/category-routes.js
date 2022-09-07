bc9931ba997b9419cdcdc476fc2ef0dcd1d3a26a
Sierra Nutt <sierra.nutt@outlook.com>
2022-09-06 22:27:46 -0600

    adding comments

--- Main/routes/api/category-routes.js
+++ Main/routes/api/category-routes.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
+
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
   Category.findAll({
     include: [Product],
   })
@@ -12,6 +15,8 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
   Category.findOne({
     where: {
       id: req.params.id,
@@ -23,12 +28,14 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+    // create a new category
   Category.create(req.body)
     .then((category) => res.status(200).json(category))
     .catch((err) => res.status(400).json(err));
 });
 
 router.put('/:id', (req, res) => {
+    // update a category by its `id` value
   Category.update(req.body, {
     where: {
       id: req.params.id,
@@ -39,6 +46,7 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+    // delete a category by its `id` value
   Category.destroy({
     where: {
       id: req.params.id,
